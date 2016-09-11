c456b6b216df0347556b7a650e21f441ec7358ce
Evan Mattson <me@aaemnnost.tv>
2016-09-12 00:57:04 +0400

    tweak required_wp_cli_version passed to template

--- inc/ScaffoldPackageCommand.php
+++ inc/ScaffoldPackageCommand.php
@@ -172,7 +172,7 @@ class ScaffoldPackageCommand {
 			'package_short_name'  => $bits[1],
 			'package_name_border' => str_pad( '', strlen( $composer_obj['name'] ), '=' ),
 			'package_description' => $composer_obj['description'],
-			'required_wp_cli_version' => ! empty( $composer_obj['require']['wp-cli/wp-cli'] ) ? str_replace( '~', 'v', $composer_obj['require']['wp-cli/wp-cli'] ) : 'v0.23.0',
+			'required_wp_cli_version' => ! empty( $composer_obj['require']['wp-cli/wp-cli'] ) ? str_replace( array( '~', '^' ), 'v', $composer_obj['require']['wp-cli/wp-cli'] ) : 'v0.23.0',
 			'shields'             => '',
 			'has_commands'        => false,
 			'wp_cli_update_to_instructions' => 'the latest stable release with `wp cli update`',
