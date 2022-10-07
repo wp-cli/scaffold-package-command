dec33bca46c7d372bfed73feb49de67956cbea92
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-10-07 06:13:53 -0700

    Place the `config.yml` to the correct path

--- src/ScaffoldPackageCommand.php
+++ src/ScaffoldPackageCommand.php
@@ -678,7 +678,7 @@ EOT;
 				$travis_append = file_get_contents( $package_dir . 'travis-append.yml' );
 			}
 		} elseif ( 'circle' === $assoc_args['ci'] ) {
-			$copy_source[ $package_root ]['.circleci/config.yml'] = $package_dir;
+			$copy_source[ $package_root ]['.circleci/config.yml'] = $package_dir . 'circleci/';
 		}
 
 		$files_written = [];
@@ -711,6 +711,10 @@ EOT;
 					continue;
 				}
 				$files_written[] = $file_path;
+
+				if ( ! is_dir( dirname( $file_path ) ) ) {
+					Process::create( Utils\esc_cmd( 'mkdir %s', dirname( $file_path ) ) )->run();
+				}
 
 				Process::create( Utils\esc_cmd( 'touch %s', $file_path ) )->run();
 				file_put_contents( $file_path, $contents );
